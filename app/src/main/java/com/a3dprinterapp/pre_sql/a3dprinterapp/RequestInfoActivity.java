45065818ed2b3f4b3496cf74adeb6621942bda69
Znowy <jknudsen99@gmail.com>
2018-11-09 17:57:14 -0800

    Added Field Validation
    -Field validation for name and project description

--- app/src/main/java/com/a3dprinterapp/pre_sql/a3dprinterapp/RequestInfoActivity.java
+++ app/src/main/java/com/a3dprinterapp/pre_sql/a3dprinterapp/RequestInfoActivity.java
@@ -2,6 +2,8 @@ package com.a3dprinterapp.pre_sql.a3dprinterapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,10 +15,30 @@ public class RequestInfoActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requestinfo);
+
         EditText userName = findViewById(R.id.editName);
+        userName.addTextChangedListener(new TextValidator(userName) {
+            @Override public void validate(TextView textView, String text) {
+                if (!text.matches("[ a-zA-Z]+$"))
+                {
+                    textView.setError("Please only enter your name.");
+                }
+            }
+        });
+
         EditText userFilepath = findViewById(R.id.editFilepath);
+
         TextView userDate = findViewById(R.id.textDate);
+
         EditText userDescription = findViewById(R.id.editDescription);
+        userDescription.addTextChangedListener(new TextValidator(userDescription) {
+            @Override public void validate(TextView textView, String text) {
+                if (text.length() < 5)
+                {
+                    textView.setError("Please provide a description of your project.");
+                }
+            }
+        });
     }
 
     public void AddAttachment(View view)
@@ -32,5 +54,34 @@ public class RequestInfoActivity extends AppCompatActivity
     public void SubmitRequest(View view)
     {
         // Submit the print request
+    }
+
+    //Source: https://stackoverflow.com/questions/33072569/best-practice-input-validation-android
+    public abstract class TextValidator implements TextWatcher {
+        private final TextView textView;
+
+        public TextValidator(TextView textView) {
+            this.textView = textView;
+        }
+
+        public abstract void validate(TextView textView, String text);
+
+        @Override
+        final public void afterTextChanged(Editable s) {
+            String text = textView.getText().toString();
+            validate(textView, text);
+        }
+
+        @Override
+        final public void
+        beforeTextChanged(CharSequence s, int start, int count, int after) {
+            /* Needs to be implemented, but we are not using it. */
+        }
+
+        @Override
+        final public void
+        onTextChanged(CharSequence s, int start, int before, int count) {
+            /* Needs to be implemented, but we are not using it. */
+        }
     }
 }

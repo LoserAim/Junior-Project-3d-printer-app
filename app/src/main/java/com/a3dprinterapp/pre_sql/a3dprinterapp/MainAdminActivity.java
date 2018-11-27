38fef154733023d904e6860c3ae18bc1c80ec206
Bradley Bergstrom <35245874+BradBergstrom@users.noreply.github.com>
2018-11-27 15:36:21 -0800

    Fixed the GUI
    for real this time... as long as it merges...

--- app/src/main/java/com/a3dprinterapp/pre_sql/a3dprinterapp/MainAdminActivity.java
+++ app/src/main/java/com/a3dprinterapp/pre_sql/a3dprinterapp/MainAdminActivity.java
@@ -56,14 +56,14 @@ public class MainAdminActivity extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabAdmin);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabAdmin);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
     }
 
